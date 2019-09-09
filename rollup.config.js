144044a3d66cc0100d563dd1284873d85bd48275
EqualMa <equalma@outlook.com>
2019-09-09 17:00:48 +0000

    setup rollup for src

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,11 @@
 import * as glob from "glob";
 import { DEFAULT_EXTENSIONS } from "@babel/core";
 import typescript from "rollup-plugin-typescript2";
-import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
 import autoExternal from "rollup-plugin-auto-external";
-import { terser } from "rollup-plugin-terser";
 import { eslint } from "rollup-plugin-eslint";
-import visualizer from "rollup-plugin-visualizer";
 
 const entries = [...glob.sync("src/*.ts"), ...glob.sync("src/*/index.ts")];
-
-const namedExports = {};
 
 export default {
   // cache: false,
@@ -23,8 +17,6 @@ export default {
   plugins: [
     eslint(),
 
-    bundle && resolve(),
-
     typescript({
       tsconfig: "./tsconfig.prod.json",
       rollupCommonJSResolveHack: true,
@@ -32,23 +24,11 @@ export default {
       clean: true,
     }),
 
-    bundle
-      ? commonjs({
-          include: /node_modules/,
-          namedExports,
-        })
-      : autoExternal(),
+    autoExternal(),
 
     babel({
       extensions: [...DEFAULT_EXTENSIONS, ".ts", ".tsx"],
       exclude: "node_modules/**",
     }),
-
-    min && terser(),
-
-    !(visualHtmlFile in generatedVisual) &&
-      visualizer({
-        filename: (generatedVisual.visualHtmlFile = visualHtmlFile),
-      }),
-  ].filter(_ => _),
+  ],
 };
