64ae52e4a2c35f00011d1cc7071c30175d35e996
EqualMa <equalma@outlook.com>
2019-09-10 01:36:57 +0800

    migrate to express and publish after build (#2)
    * init
    
    * gen yarn.lock
    
    * typing
    
    * add
    
    * setup rollup for src
    
    * build static files
    
    * demo
    
    * update node engine

--- iam-service/middlewares.js
+++
@@ -1,5 +0,0 @@
-exports.default = [
-  async ctx => {
-    ctx.body = "Hello World from iam service at " + new Date().toString();
-  }
-];
---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import * as glob from "glob";
+import { DEFAULT_EXTENSIONS } from "@babel/core";
+import typescript from "rollup-plugin-typescript2";
+import babel from "rollup-plugin-babel";
+import autoExternal from "rollup-plugin-auto-external";
+import { eslint } from "rollup-plugin-eslint";
+import generatePackageJson from "rollup-plugin-generate-package-json";
+import copy from "rollup-plugin-copy";
+
+const entries = [...glob.sync("src/*.ts"), ...glob.sync("src/*/index.ts")];
+
+export default {
+  // cache: false,
+  input: entries,
+  output: {
+    format: "cjs",
+    dir: "dist/iam-service",
+  },
+  external: ["body-parser"],
+  plugins: [
+    eslint(),
+
+    typescript({
+      tsconfig: "./tsconfig.prod.json",
+      rollupCommonJSResolveHack: true,
+      objectHashIgnoreUnknownHack: true,
+      clean: true,
+    }),
+
+    autoExternal(),
+
+    babel({
+      extensions: [...DEFAULT_EXTENSIONS, ".ts", ".tsx"],
+      exclude: "node_modules/**",
+    }),
+
+    copy({
+      targets: [{ src: "stack.yml", dest: "dist" }],
+    }),
+
+    generatePackageJson({
+      outputFolder: "dist",
+    }),
+  ],
+};
